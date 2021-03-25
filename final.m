52df40772936840152d5cbd474e5b70bff8aa71f
kurumint <claudia.bonanno@yahoo.it>
2021-03-25 15:39:07 +0100

    clean code and side images

--- final.m
+++ final.m
@@ -1,63 +1,147 @@
 clear all 
+close all
 
 %legge l'immagine della quale si vuole trascrivere il contenuto
 I = imread('eng.png');
-%I = imread('italic.png');
+%I = imread('gradient.png');
 
-%I = imnoise(I,'gaussian');
-%I = imnoise(I,'salt & pepper');
-%I = imtransform(I,maketform('affine',[1 0 0; .5 1 0; 0 0 1]));
+%I = imnoise(I,'gaussian', 0.8, 0.2);
+%I = imnoise(I,'salt & pepper', 0.025);
+%I = imrotate(I, 5);
+
+originale = I;
+figure, imshow(I);
+%imwrite(I,'0.png');
 
 %porta tale immagine a scala di grigi
 I = rgb2gray(I);
+%figure, imshow(I);
+%imwrite(I,'1.png');
 
 %operatore bottom hat per estrarre il testo (scuro) dallo sfondo (chiaro)
-e = strel('disk', 5);
+e = strel('disk', 7);
 I = imbothat(I, e);
+%figure, imshow(I);
+%imwrite(I,'2.png');
 
 %sogliatura con otsu e binarizzazione dell'immagine
 T = graythresh(I);
 I = imbinarize(I, T);
+%figure, imshow(I);
+%imwrite(I,'3.png');
+
+copia = I;
 
 %noise reduction
-x=[0 1 1 1 0; 1 1 1 1 1; 0 1 1 1 0];
-y=[1 1 1 1;1 1 1 1;1 1 1 1];
-I=imdilate(I,x);
-I=imerode(I,y);
+x = [0 1 1 1 0; 1 1 1 1 1; 0 1 1 1 0];
+y = [1 1 1 1 ; 1 1 1 1; 1 1 1 1];
+I = imdilate(I,x);
+I = imerode(I,y);
+%figure, imshow(I);
+%imwrite(I,'4.png');
 
-imshow(I);
+I = wiener2(I,[3 3]);
+%figure, imshow(I);
+%imwrite(I,'5.png');
+
+%vengono trovate le linee con la trasformata di hough
+[H,theta,rho] = hough(I);
+peaks = houghpeaks(H,1);
+lines = houghlines(I,theta,rho,peaks);
+
+%ci interessa l'angolo più frequente di queste linee (che sarebbe l'angolo
+%di cui è ruotata l'immagine). prendendo il primo supponiamo che tutte
+%abbiano lo stesso angolo all'interno dell'immagine
+angle = lines(1).theta;
+correction_angle = 0;
+if angle > 0
+    correction_angle = angle - 90;
+elseif angle < 0
+    correction_angle = 90 + angle;
+end
+
+if angle ~= 0
+    I = imrotate(I, correction_angle);
+end
+%figure, imshow(I);
+%imwrite(I,'6.png');
 
 %vengono prese le singole lettere
-stats = regionprops(I, 'BoundingBox');
+stats = regionprops(I, 'BoundingBox', 'Area');
 
 %in fase di acquisizione, non è detto che tutte le boundingbox trovate
-%abbiano la stessa area. per questo motivo, ognuna viene ridimensionata a
-%16x16. 
+%abbiano la stessa area (soprattutto in un caso in cui siano presenti sia
+%upper case che lower case). per questo motivo, ognuna viene ridimensionata
+%a 16x16. 
 
 %viene creata una matrice per contenere le singole lettere in cui ogni riga
 %corrisponde ad una lettera. per visualizzarne una, è necessario fare
 %imshow della riga "reshaped" a 16x16. 
+letters = [];
 textletters = zeros(0,256);
+
+area = [stats.Area];
+mean = mean(area);
+
+%figure, imshow(I)
+%hold on
+%for i = 1 : length(stats)
+%    BB = stats(i).BoundingBox;
+%    rectangle('Position', [BB(1), BB(2), BB(3), BB(4)],'EdgeColor','r', 'LineWidth', 3)
+%end
+
+realboxes = [];
+rletters = [];
 for i = 1 : length(stats)
-    BB = stats(i).BoundingBox;
-    newletter = imcrop(I,[BB(1), BB(2), BB(3), BB(4)]);
-    resizedletter = imresize(newletter, [16,16]);
-    finalletter = reshape(resizedletter, 1, 256);
-    textletters = [textletters; double(finalletter)];
+    %se l'area della bounding box è molto minore della dimensione media,
+    %probabilmente abbiamo preso rumore e non una vera lettera, passiamo
+    %avanti
+    if stats(i).Area > 0.5*mean
+        BB = stats(i).BoundingBox;
+        newletter = imcrop(I,[BB(1), BB(2), BB(3), BB(4)]);
+        realboxes{end+1} = [BB(1), BB(2), BB(3), BB(4)];
+        letters{end+1} = newletter;
+        resizedletter = imresize(newletter, [16,16]);
+        rletters{end+1} = resizedletter;
+        finalletter = reshape(resizedletter, 1, 256);
+        textletters = [textletters; double(finalletter)];
+    end
 end 
 
+%figure, imshow(I)
+%hold on
+%for i = 1 : length(realboxes)
+%    rectangle('Position', realboxes{i},'EdgeColor','r', 'LineWidth', 3)
+%end
+
+%figure
+%for i = 1 : size(letters, 2)
+%    subplot(1,size(letters, 2),i);
+%    imshow(letters{i});
+%end
+
+%figure
+%for i = 1 : size(rletters, 2)
+%    subplot(1,size(rletters, 2),i);
+%    imshow(rletters{i});
+%end
+
+%
+%
+%
+
 %costruzione del "database" contenente le lettere su cui baseremo il
-%confronto.
-%le operazioni effettuate sono simili a quelle effettuate per l'immagine
-%che vogliamo analizzare. 
+%confronto. 
 alphabet = imread('upper.png');
 alphabet = rgb2gray(alphabet);
 alphabet = imbothat(alphabet, e);
 Ta = graythresh(alphabet);
 alphabet = imbinarize(alphabet, Ta);
+alphabet = wiener2(alphabet,[3 3]);
+
 astats = regionprops(alphabet, 'BoundingBox');
 
-%dato che nell'immagine l'alfabero è distribuito su più righe, bisogna
+%dato che nell'immagine l'alfabeto è distribuito su più righe, bisogna
 %ordinare opportunamente le boundingbox per ottenere una lettura
 %sinistra->destra, alto->basso. 
 apositions = zeros(0,6);
@@ -79,8 +163,9 @@ end
 %lettera da classificare rispetto alla j-esima lettera presente nel
 %database. ogni riga, quindi, contiene tutte le distanze della i-esima
 %lettera da tutte quelle del database. 
-m = zeros(length(stats), length(astats));
-for i = 1 : length(stats)
+tldim = size(textletters,1);
+m = zeros(tldim, length(astats));
+for i = 1 : tldim
     for j = 1 : length(astats)
         i_row_letters = textletters(i,:);
         j_row_database = database(j,:);
@@ -94,16 +179,19 @@ end
 
 %il risultato viene stampato
 s = 'ABCDEFGHIJKLMNOPRSTUVWXYZQ';
-for i = 1 : length(stats)
-    fprintf(s(minIndices(i,1)));
+output = "";
+for i = 1 : tldim
+    output = strcat(output,s(minIndices(i,1)));
 end
+output
 fprintf('\n');
 
-%utile per la visualizzazione
-%for i = 1:20
-%    subplot(4,5,i);
-%    imshow(imds.Files{perm(i)});
-%end
+%misuriamo la distanza di edit tra la stringa vera e quella ottenuta
+real = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG";
+distanza = editDistance(real,output)
+normalized = distanza/max([strlength(real), strlength(output)])
 
-%pca: sottrarre il valore medio e poi applicare la funzione per ridurre la
-%dimensionalità
+txt = ocr(originale)
+confronto = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG';
+d_ocr = editDistance(txt.Text,confronto)-2
+n_ocr = d_ocr/max([strlength(txt.Text), strlength(confronto)])
